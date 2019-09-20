337ca57fb66e21327b00d32f22e8db11a51050da
Luis Gonzalez <luisalfredog@outlook.com>
2019-09-20 16:58:40 -0600

    Path users disponibility on api

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import config from './config'
 const { server } = config
 
 export default function getDisponibility(){
-  return fetch(`${server}`)
+  return fetch(`${server}/users/disponibility`)
           .then(res => res.json())
           .catch(error => { errors })
   }
