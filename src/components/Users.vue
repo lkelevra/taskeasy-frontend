c9a11afa99cd231313065a5f7f5106a6cbb08be3
Luis Gonzalez <luisalfredog@outlook.com>
2019-09-20 16:55:08 -0600

    User Component

---
+++ src/components/Users.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+    span(class="username") {{user.name}}
+</template>
+<script>
+export default {
+  name: 'users',
+  props: [ 'user' ],
+}
+</script>
+<style lang="stylus">
+.username
+  background-color #67cdd8
+  box-box-sizing border-box
+  padding 0.5rem
+  margin 1vw
+  border-radius 1vw
+  color white
+  font-size 1rem
+</style>
