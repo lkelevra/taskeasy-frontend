7b03b5495dc35ecdd8ccf6ec6d21cc64ff8e362e
Luis Gonzalez <luisalfredog@outlook.com>
2019-09-20 16:51:21 -0600

    Title vue app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  #app
+    h1 Availivity List
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+        Hour
+  },
+  data () {
+    return {
+      schedules:[
+      ]
+    }
+  },
+  
 }
 </script>
+<style lang="stylus">
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
